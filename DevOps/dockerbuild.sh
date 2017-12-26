a55ae99df60df33022a218023a0e7a3f7e47fae4
Shwshank <shwshank3@gmail.com>
2017-12-26 13:46:58 +0530

    updated dist and dockerbuild.sh

--- DevOps/dockerbuild.sh
+++ DevOps/dockerbuild.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+
 TIMESTAMP=`TZ=IST-5:30 date +%F-%H%M`
+
 GITHUB_LINK="https://github.com/Shwshank/QCI-Collect-frontend.git"
+
 DOCKERNAME="Collect-Frontend"
+
 IMAGENAME="qci-collect-front"
-REPONAME="QCI-Collect-frontend"
+
+REPONAME="collect-frontend-pipeline"
+
 
 git clone $GITHUB_LINK
-mv dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
-mv $REPONAME/dist/ .
-rm -rf $REPONAME
-docker stop $DOCKERNAME
+
+mv /var/lib/jenkins/workspace/collect-frontend/dist/ /home/ubuntu/dist_backups/collect/dist_$TIMESTAMP
+
+mv /var/lib/jenkins/workspace/collect-frontend-pipeline/dist /var/lib/jenkins/workspace/collect-frontend/
+
 docker rm $DOCKERNAME
+
 docker rmi $IMAGENAME
-docker build -t $IMAGENAME .
+
+docker build -t $IMAGENAME /var/lib/jenkins/workspace/collect-frontend/.
+
 docker run -dt --name $DOCKERNAME $IMAGENAME
