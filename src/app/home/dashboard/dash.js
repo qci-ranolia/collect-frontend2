aa09765c450764ce7eece04d2cfe2a90ce7e7c1c
deepak-ranolia <d.ranolia92@gmail.com>
2017-12-27 20:47:23 +0530

    form page design updated

--- src/app/home/dashboard/dash.js
+++ src/app/home/dashboard/dash.js
@@ -13,8 +13,19 @@ $(function(){
         $(this).removeClass("active");
         $(this).parents("body").addClass("left-menu-close");
     });
+    //TESTING PHASE, more improvements in later versions will be seen
+    $(document).keypress(function(e) {
+        var a = $(".modal");
+        //var b = a.find(".modal-footer");
+        if ( a.hasClass('show') ){
+            if(e.which == 13) {
+                //b.children(".btn-primary").trigger("click");
+                alert('New Implementation being tested right now just for you.........You pressed enter! keynumber is 13.....');
+            }
+        }
+    });
 });
-//  **Navigation Mobile js
+//**Navigation Mobile js
 $(function(){
 "use strict";
     if ( window.innerWidth <= 1024 ) {
